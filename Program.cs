aa21866090855e739efb089b12ce5ef4bbbe864e
P0wderGang3r <P0wderGang3r@outlook.com>
2023-01-06 16:25:42 +0300

    Added basic CORS policioes

--- Program.cs
+++ Program.cs
@@ -4,6 +4,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyMethod();
+                      });//AllowAnyOrigin() - небезопасно. В релизе заменить на WithOrigins("http://85.143.223.149:Client_Port")
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -14,6 +24,8 @@ builder.Services.AddScoped<IRedRepository, RedRepository>();
 DBConnection.StartupInitNewConnection(builder.Configuration);
 
 var app = builder.Build();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
