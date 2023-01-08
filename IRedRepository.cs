13933c23fcf2b313566b62e312525e1168f1db79
Nlexxs <121118327+Nlexxs@users.noreply.github.com>
2023-01-09 00:54:08 +0300

    Add files via upload

--- IRedRepository.cs
+++ IRedRepository.cs
@@ -1,8 +1,78 @@
-﻿namespace server_red
+﻿using server_red.Models;
+
+namespace server_red
 {
     public interface IRedRepository
     {
         // Авторизация
-        List<string> SignIn(string username, string password);
+        //List<string> SignIn(string username, string password);
+        string SignIn(string username, string password);
+
+        // Создание аккаунта
+        string CreateAccount(string username, string password, string question, string answer);
+
+        // Восстановление пароля - запрос никнейма
+        ReqNick ReqNick(string username);
+
+        // Восстановление пароля - ответ на контрольный вопрос
+        string AnsQues(string token, string answer);
+
+        // Изменить пароль
+        string ChangePass(string token, string newPass);
+
+        // Получение списка друзей
+        List<User> GetFriendlist(string cur_session);
+
+        // Получение списка уведомлений
+        List<Notif> GetNotiflist(string cur_session);
+
+        // Получение иконки, никнейма, количества очков
+        UserScore GetUserScore(string cur_session);
+
+        // Поиск пользователя
+        User UserSearch(string username);
+
+        // Добавить пользователя в друзья
+        int AddFriend(string cur_session, int f_id);
+
+        // Удалить пользователя из друзей
+        int DeleteFriend(string cur_session, int f_id);
+
+        // Отправить приглашеие в пользовательский матч
+        int InviteFriend(string cur_session, int f_id, int move_time, int rules_id);
+
+        // Получение цвета текущего хода
+        string GetActiveColor (string cur_session);
+
+        // Получение флага ударного хода
+        int GetBeatFlag(string cur_session);
+
+        // Получение всех белых шашек
+        List<Checker> SessionCheckersWhite(string cur_session);
+
+        // Получение всех черных шашек
+        List<Checker> SessionCheckersBlack(string cur_session);
+
+        //Изменение цвета шашек текущего хода
+        int SetActiveColor(string cur_session, string color);
+
+        //Задание флага ударного хода
+        int SetBeatFlag(string cur_session, int beat_flag);
+
+        // Обновление списка шашек (вызывается последовательно для каждой шашки)
+        int UpdateCheckersField(string cur_session, string color, int fx, int fy, int king, int beaten, bool delete_old);
+
+        // Принять приглашение в пользовательский матч
+        int AcceptMatch(string cur_session, string org_nick);
+
+        // Отозвать приглашение в пользовательский матч
+        int RevokeMatch(string cur_session, int f_id);
+
+        // Отклонить приглашение в пользовательский матч
+        int RejectMatch(string cur_session, string org_nick);
+
+        // Сдаться
+        int GiveUp(string cur_session);
+
     }
 }
