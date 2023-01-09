b64e9b2496508afd7b97b9b0af17e9f2ccfd7bce
Nlexxs <121118327+Nlexxs@users.noreply.github.com>
2023-01-10 02:29:18 +0300

    Add files via upload

---
+++ Models/Opponent.cs
@@ -0,0 +1,10 @@
+﻿namespace server_red.Models
+{
+    public class Opponent
+    {
+        public int uid { get; set; }
+        public string nick { get; set; }
+        public string photo { get; set; }
+        public int score { get; set; }
+    }
+}
