ebc680fb24f6a0b93503dc82d3639247e66dc327
Nlexxs <121118327+Nlexxs@users.noreply.github.com>
2023-01-10 14:25:07 +0300

    Add files via upload

---
+++ Models/GameInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace server_red.Models
+{
+    public class GameInfo
+    {
+        public string? nick1 { get; set; }
+        public int score1 { get; set; }
+        public string? nick2 { get; set; }
+        public int score2 { get; set; }
+        public int move_time { get; set; }
+        public int rules_id { get; set; }
+    }
+}
---
+++ Models/Move.cs
@@ -0,0 +1,8 @@
+﻿namespace server_red.Models
+{
+    public class Move
+    {
+        public int num { get; set; }
+        public string? note { get; set; }
+    }
+}
