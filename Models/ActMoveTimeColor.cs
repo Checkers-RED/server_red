5b8f31aed5fbd674cb21487676070d97f881e990
Nlexxs <121118327+Nlexxs@users.noreply.github.com>
2023-01-25 22:12:25 +0300

    Add files via upload

---
+++ Models/ActMoveTimeColor.cs
@@ -0,0 +1,9 @@
+﻿namespace server_red.Models
+{
+    public class ActMoveTimeColor
+    {
+        public string? color { get; set; } // цвет текущео хода
+        public string? dttime { get; set; } // дата-время начала текущего хода
+
+    }
+}
